897eb102993980e7498c95ffdabc9d7e860b40cb
Unknown <i.runets95@gmail.com>
2018-02-27 22:53:09 +0300

    smart-calculator: solved

--- src/index.js
+++ src/index.js
@@ -1,26 +1,41 @@
 class SmartCalculator {
   constructor(initialValue) {
     // your implementation
+    this.result = initialValue;
   }
 
   add(number) {
     // your implementation
+    this.result += '+' + number;
+    return this;
   }
-  
+
   subtract(number) {
     // your implementation
+    this.result += '-' + number;
+    return this;
   }
 
   multiply(number) {
     // your implementation
+    this.result += '*' + number;
+    return this;
   }
 
   devide(number) {
     // your implementation
+    this.result += '/' + number;
+    return this;
   }
 
   pow(number) {
     // your implementation
+    this.result += "**" + number;
+    return this;
+  }
+
+  valueOf() {
+    return eval(this.result);
   }
 }
 
